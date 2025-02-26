89ff9f2840a603bdb43e26b15776777f4b4894d4
Kate Tang <kate.tang06@gmail.com>
2025-02-26 10:56:32 -0500

    smth

--- main.cpp
+++ main.cpp
@@ -4,22 +4,35 @@
 #include <FEHLCD.h>
 #include <FEHServo.h>
 
+FEHMotor left_motor(FEHMotor::Motor0,9.0); 
+FEHMotor right_motor(FEHMotor::Motor2,9.0); 
+
+DigitalInputPin fr_micro(FEHIO::P0_7);
+DigitalInputPin br_micro(FEHIO::P0_5);
+DigitalInputPin fl_micro(FEHIO::P3_7);
+DigitalInputPin bl_micro(FEHIO::P3_6);
+
+#define MOTORPOWER 40
+
 int main(void)
 {
     float x,y;
     while(!LCD.Touch(&x,&y)){};
 
-    FEHMotor left_motor(FEHMotor::Motor0,9.0); 
-    FEHMotor right_motor(FEHMotor::Motor2,9.0); 
-
-    DigitalInputPin fr_micro(FEHIO::P0_7);
-    DigitalInputPin br_micro(FEHIO::P0_5);
-    DigitalInputPin fl_micro(FEHIO::P3_7);
-    DigitalInputPin bl_micro(FEHIO::P3_6);
 }
 
-    //Peformance Test 1
-    
+//Peformance Test 1
+void peformanceTest1(void){
+    //first part
+
+    //second part: ramp
+    setMotorSpeed(MOTORPOWER, MOTORPOWER);
+}
+
+void setMotorSpeed(int leftSpeed, int rightSpeed){
+    left_motor.SetPercent(leftSpeed);
+    right_motor.SetPercent(rightSpeed);
+}
 
 
     //Exploration 02
