fa05534cb6607953831847c9b1f377a13b4f4a68
fullerweevil <theaidenjohn@gmail.com>
2025-02-26 12:07:27 -0500

    Added comments
    Added extra comments to the first performance test so as not to get confused as to what part of it we are coding.

--- main.cpp
+++ main.cpp
@@ -23,10 +23,24 @@ int main(void)
 
 //Code for Peformance Test 1
 void peformanceTest1(void){
-    //first part
+    /*  FIRST PART: 
+        Starting with robots trailing edge AGAINST the wall to the right of the starting area. 
+        Then, drive until you hit the other wall on the left side WITHOUT touching compost bin or tree stump
+    */
+   setMotorSpeed(MOTORPOWER, MOTORPOWER);
 
-    //second part: ramp
-    setMotorSpeed(MOTORPOWER, MOTORPOWER);
+
+    /*  SECOND PART: 
+        Place robot arbitrarily on the lower level at the bottom of the ramp with NO driving surfaces
+        touching any part of the ramp.
+        Then, the robot drives up the ramp to the upper level and ends with no drive surfaces touching any part of the ramp.
+    */
+
+    /*  EXTRA CREDIT
+        Robot is able to drive back down the ramp to the lower level with no drive surfaces touching any part of the course/ramp. 
+        Must be done WITHOUT interruption AFTER having driven up to the upper level.
+    */
+
 }
 
 //Sets the left and right motor speed
