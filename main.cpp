c6a2441df3a8edbdda75af430188ea088c1299e4
fullerweevil <theaidenjohn@gmail.com>
2025-03-06 16:44:50 -0500

    Went back on changes dog

--- main.cpp
+++ main.cpp
@@ -97,9 +97,9 @@ void turn(double angle){
     left_encoder.ResetCounts();
 
     if(angle > 0){
-        setMotorSpeed(MOTORPOWER + 1, -1 * MOTORPOWER); //turn right
+        setMotorSpeed(MOTORPOWER, -1 * MOTORPOWER); //turn right
     }else{
-        setMotorSpeed(-1 * MOTORPOWER - 1, MOTORPOWER); //turn left
+        setMotorSpeed(-1 * MOTORPOWER, MOTORPOWER); //turn left
     }
 
      //Motors run while the average of the left and right encoder is less than counts
